dbbf7c69250cfa8c7b5d95e853b19a07c84de896
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-08-10 23:31:42 +0400

    Enable pretty URLs

--- config/web.php
+++ config/web.php
@@ -43,13 +43,13 @@ $config = [
             ],
         ],
         'db' => $db,
-/*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
-        ],*/
+        ],
 
     ],
     'params' => $params,
